f72890de307cbaae23de861458ccb1484ed0206d
Halfdemigod1987 <theandreyfirst@yandex.ru>
2022-07-29 14:53:15 +0300

    feat: Добавить покрытие тестапи класса ReversePolishNotationCalculator

--- src/test/java/notation/ReversePolishNotationCalculatorTest.java
+++ src/test/java/notation/ReversePolishNotationCalculatorTest.java
@@ -1,11 +1,51 @@
 package notation;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReversePolishNotationCalculatorTest {
 
+    private static ReversePolishNotationCalculator calculator;
+
+    @BeforeAll
+    public static void initCalculator() {
+        calculator = new ReversePolishNotationCalculator();
+    }
+
     @Test
     public void shouldCalculateAddition() {
-
+        assertEquals(7, calculator.calculatePolishNotation("4 3 +"));
     }
+
+    @Test
+    public void shouldCalculateSubtraction() {
+        assertEquals(-1, calculator.calculatePolishNotation("2 3 -"));
+    }
+
+    @Test
+    public void shouldCalculateMultiplication() {
+        assertEquals(6, calculator.calculatePolishNotation("2 3 *"));
+    }
+
+    @Test
+    public void shouldCalculateWithAdditionalSpaces() {
+        assertEquals(7, calculator.calculatePolishNotation(" 4   3  + "));
+    }
+
+    @Test
+    public void shouldCalculateWithNegativeDigits() {
+        assertEquals(1, calculator.calculatePolishNotation("-4 5 +"));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenIllegalOperator() {
+        NumberFormatException ex = assertThrows(
+                NumberFormatException.class,
+                () -> calculator.calculatePolishNotation("-4 5 /")
+        );
+    }
+
 }
